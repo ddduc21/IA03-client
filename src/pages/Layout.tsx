fb7e3a5add4e0c43a66c74804b1f85fa42aab0d8
duck420i69 <88267139+duck420i69@users.noreply.github.com>
2024-11-05 21:14:36 +0700

    commit 2

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -4,9 +4,12 @@ const Layout = () => {
 	return (
 		<>
 			<nav className="flex flex-row justify-between w-screen bg-primary">
-				<div className="flex flex-row justify-center content-center">
-					<h1 className="text-center">App</h1>
-				</div>
+				<Link
+					to="/"
+					className="px-3 py-2 bg-primary text-primary-foreground hover:bg-secondary/20 border-l border-secondary/20"
+				>
+					Home
+				</Link>
 				<div className="flex flex-row justify-end">
 					<Link
 						to="/user/login"
